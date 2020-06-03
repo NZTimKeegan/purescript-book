66b5574d0226f0223f9a7f4ed3bfd70b55e58d23
tim <tim@pop-os.localdomain>
2020-06-03 23:38:05 +1200

    Fix Chapter 2 warnings

--- exercises/chapter2/src/Main.purs
+++ exercises/chapter2/src/Main.purs
@@ -1,7 +1,8 @@
 module Main where
 
-import Prelude
+import Prelude (Unit, (*), (+))
 
+import Effect
 import Effect.Console (logShow)
 import Math (sqrt)
 
@@ -9,4 +10,5 @@ import Math (sqrt)
 diagonal :: Number -> Number -> Number
 diagonal w h = sqrt (w * w + h * h)
 
+main :: Effect Unit
 main = logShow (diagonal 3.0 4.0)
